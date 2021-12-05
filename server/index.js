2ebc073867404d616cfb8e41240181abba264af0
Chupriarti <wallx13@gmail.com>
2021-12-05 20:31:21 +0300

    add get for testing

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'WORKING!'});
+});
 
 const start = async () => {
     try {
