28651b7d855e7a5d484001b736fa247bbe3260dc
Chupriarti <wallx13@gmail.com>
2021-12-10 22:20:01 +0300

    refactoring

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,15 @@ const ApiError = require('../error/ApiError');
 const bcrypt = require('bcrypt');
 const {User, Basket} = require('../models/models');
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign
+    (
+        {id, email, role}, 
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
 class UserController {
     async registration (req, res) {
         const {email, password, role} = req.body;
@@ -17,12 +26,7 @@ class UserController {
         const hashPassword = await bcrypt.hash(password, 5);
         const user = await User.create({email, role, password: hashPassword});
         const basket = await Basket.create({userId: user.id});
-        const jwt = jwt.sign
-        (
-            {id: user.id, email, role}, 
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        )
+        const jwt = generateJwt(user.id, user.email, user.role);
         return res.json({token});
     }
 
