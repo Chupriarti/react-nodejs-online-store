eef834133c233e374637fdc0b03e5d16e225d767
Chupriarti <wallx13@gmail.com>
2021-12-10 22:04:55 +0300

    prepare user registration api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,15 @@
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const {User, Basker} = require('../models/models');
 
 class UserController {
     async registration (req, res) {
-
+        const {email, password, role} = req.body;
+        if (!email || !password) {
+            return next(ApiError.badRequest("Wrong email or password"));
+        }
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, role, password});
     }
 
     async login (req, res) {
