ab7270a6c2736161ba8f991f8aaa91068b16d884
Chupriarti <wallx13@gmail.com>
2021-12-10 22:06:36 +0300

    add check if user exist

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,6 +8,12 @@ class UserController {
         if (!email || !password) {
             return next(ApiError.badRequest("Wrong email or password"));
         }
+
+        const candidate = await User.findAll({where: {email}});
+        if (candidate){
+            return next(ApiError.badRequest("User is already exist"));
+        }
+
         const hashPassword = await bcrypt.hash(password, 5);
         const user = await User.create({email, role, password});
     }
