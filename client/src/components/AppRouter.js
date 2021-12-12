03ba4d9e4a1f1dbc17d8b6cb4b87c23780345aea
Chupriarti <wallx13@gmail.com>
2021-12-12 18:21:57 +0300

    add AppRouter

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+import { authRoutes, publicRoutes } from '../routes';
 
 const AppRouter = () => {
+    const isAuth = false;
+
     return (
-        <div>
-            AppRouter
-        </div>
+        <Routes>
+            {isAuth && authRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />} exact />
+            )}
+            {publicRoutes.map(({path, Component}) => 
+                <Route key={path} path={path} element={<Component />} exact />
+            )}
+        </Routes>
     )
 }
 
