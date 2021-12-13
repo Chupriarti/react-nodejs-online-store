df7537eefda2c0ca4d381574f94d5d385d220602
Chupriarti <wallx13@gmail.com>
2021-12-13 22:05:33 +0300

    add actions and getters for DeviceStore

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -54,4 +54,28 @@ export default class DeviceStore {
         ];
         makeAutoObservable(this);
     }
+
+    setTypes(types){
+        this._types = types;
+    }
+
+    setBrands(brands){
+        this._brands = brands;
+    }
+
+    setDevices(devices){
+        this._devices = devices;
+    }
+
+    get types () {
+        return this._types;
+    }
+
+    get brands () {
+        return this._brands;
+    }
+
+    get devices () {
+        return this._devices;
+    }
 }
