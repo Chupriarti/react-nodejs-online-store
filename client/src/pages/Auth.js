d0011bacb420e143d31fb3ea0759a68f45aa2590
Chupriarti <wallx13@gmail.com>
2021-12-15 22:23:45 +0300

    add reg or login func base on route

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Button, Card, Container, Form, Row } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import { REGISTRATION_ROUTE } from '../utils/constansts';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/constansts';
 
 const Auth = () => {
+    const location = useLocation();
+    const isLogin = location.pathname === LOGIN_ROUTE;
+
     return (
         <Container 
             className="d-flex justify-content-center align-items-center"
             style={{height: window.innerHeight - 54}}
         >
             <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">Login</h2>
+                <h2 className="m-auto">{isLogin ? "Login" : "Registration"}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control
                         className="mt-3"
@@ -21,14 +24,21 @@ const Auth = () => {
                         placeholder='Enter password...'
                     />
                     <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
-                        <div>
-                            No account? <NavLink to={REGISTRATION_ROUTE}>Registrate</NavLink>
-                        </div>
+                        {isLogin ?
+                            <div>
+                                No account? <NavLink to={REGISTRATION_ROUTE}>Registrate</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Have account? <NavLink to={LOGIN_ROUTE}>Enter</NavLink>
+                            </div>
+                        }
                         <Button 
                             variant={"outline-success"}
                         >
-                            Enter
+                            {isLogin ? "Enter" : "Registration"}
                         </Button>
+                        
                     </Row>
                 </Form>
             </Card>
