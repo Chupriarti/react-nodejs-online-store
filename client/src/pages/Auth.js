20d0c1e6da3fc763a7d4fed4f0ea5702a6ae07a8
Chupriarti <wallx13@gmail.com>
2021-12-15 21:59:36 +0300

    prepare container for auth form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Card, Container } from 'react-bootstrap';
 
 const Auth = () => {
     return (
-        <div>
-            Auth page
-        </div>
+        <Container 
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                Auth
+            </Card>
+        </Container>
     )
 }
 
