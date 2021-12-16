94b97eea9812129b97379a432dfc6161ef4ec50f
Chupriarti <wallx13@gmail.com>
2021-12-16 21:21:35 +0300

    add TypeBar to Shop page

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import TypeBar from '../components/TypeBar';
 
 const Shop = () => {
     return (
         <Container>
             <Row>
                 <Col md={3}>
-                
+                    <TypeBar />
                 </Col>
                 <Col md={9}>
                 
