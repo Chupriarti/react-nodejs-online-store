ee61670290f7571868f6a21bbbff708ee448ff1f
Chupriarti <wallx13@gmail.com>
2021-12-16 21:55:55 +0300

    add selectedBrand indication

--- client/src/components/BrandBar.js
+++ client/src/components/BrandBar.js
@@ -9,6 +9,9 @@ const BrandBar = observer(() => {
         <Row className="d-flex">
             {device.brands.map(brand => 
                 <Card
+                    style={{cursor: "pointer"}}
+                    border={brand.id === device.selectedBrand.id ? "danger" : "light"}
+                    onClick={() => device.setSelectedBrand(brand)}
                     key={brand.id}
                     className="p-3"
                 >
