6e7d3a4c95731a1b43fc66f5ae38e29afda02098
Chupriarti <wallx13@gmail.com>
2021-12-17 22:46:13 +0300

    add DeviceItem usage

--- client/src/components/DeviceList.js
+++ client/src/components/DeviceList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {observer} from 'mobx-react-lite';
 import { Row } from 'react-bootstrap';
+import DeviceItem from './DeviceItem';
+import { Context } from '..';
 
 const DeviceList = observer(() => {
+    const {device} = React.useContext(Context);
     return (
         <Row className="d-flex">
-            
+            {device.devices.map(device =>
+                <DeviceItem key={device.id} device={device} />
+            )}
         </Row>
     )
 });
