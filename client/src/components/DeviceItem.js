df3764d3f4d847c81a59e757d93fd493dba8b437
Chupriarti <wallx13@gmail.com>
2021-12-17 22:50:58 +0300

    fix brand text color

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -7,7 +7,7 @@ const DeviceItem = ({device}) => {
         <Col md={3}>
             <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
                 <Image width={150} height={150} src={device.img} />
-                <div className="mt-1 d-flex justify-content-between align-items-center">
+                <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
                     <div>Brand...</div>
                     <div className="d-flex align-items-center">
                         <div>{device.rating}</div>
