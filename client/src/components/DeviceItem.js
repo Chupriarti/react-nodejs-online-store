5145e522a6f8350510f465e51d1e79e779a8ddf7
Chupriarti <wallx13@gmail.com>
2021-12-17 22:50:06 +0300

    add DeviceItem

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Card, Col, Image } from 'react-bootstrap';
+import star from '../assets/star.png';
 
-const DeviceItem = () => {
+const DeviceItem = ({device}) => {
     return (
         <Col md={3}>
-            
+            <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
+                <Image width={150} height={150} src={device.img} />
+                <div className="mt-1 d-flex justify-content-between align-items-center">
+                    <div>Brand...</div>
+                    <div className="d-flex align-items-center">
+                        <div>{device.rating}</div>
+                        <Image width={16} height={16} src={star} />
+                    </div>
+                </div>
+                <div>{device.name}</div>
+            </Card>
         </Col>
     )
 }
