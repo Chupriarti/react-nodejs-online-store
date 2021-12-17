c600007765106f43f3c23dea4d77b7fa0ef24c4c
Chupriarti <wallx13@gmail.com>
2021-12-17 22:44:15 +0300

    prepare DeviceItem

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const DeviceItem = () => {
+    return (
+        <Col md={3}>
+            
+        </Col>
+    )
+}
+
+export default DeviceItem;
