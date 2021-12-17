36d91ac9574fb60e518cb77599005b988fecbb68
Chupriarti <wallx13@gmail.com>
2021-12-17 22:43:51 +0300

    prepare DeviceList

---
+++ client/src/components/DeviceList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {observer} from 'mobx-react-lite';
+import { Row } from 'react-bootstrap';
+
+const DeviceList = observer(() => {
+    return (
+        <Row className="d-flex">
+            
+        </Row>
+    )
+});
+
+export default DeviceList;
