c6c38b8a8ab31b68cdb6a21068f6a33a1c32420a
Chupriarti <wallx13@gmail.com>
2021-12-17 22:54:33 +0300

    fix margin

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -4,7 +4,7 @@ import star from '../assets/star.png';
 
 const DeviceItem = ({device}) => {
     return (
-        <Col md={3}>
+        <Col md={3} className={"mt-3"}>
             <Card style={{width: 150, cursor: 'pointer'}} border={"light"}>
                 <Image width={150} height={150} src={device.img} />
                 <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
