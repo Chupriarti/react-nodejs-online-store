0bd7d29d657504b8b01d345864ba4f0169c92d09
Chupriarti <wallx13@gmail.com>
2021-12-19 21:15:46 +0300

    prepare buttons on Admin Page

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
 
 const Admin = () => {
     return (
-        <div>
-            Admin page
-        </div>
+        <Container className="d-flex flex-column">
+            <Button 
+                variant={"outline-dark"} 
+                className="mt-2"
+            >
+                Add type
+            </Button>
+            <Button 
+                variant={"outline-dark"} 
+                className="mt-2"
+            >
+                Add brand
+            </Button>
+            <Button 
+                variant={"outline-dark"} 
+                className="mt-2"
+            >
+                Add device
+            </Button>
+        </Container>
     )
 }
 
