251a64a2ffbb87e6cd4b269a7fedf1cb6b9cea6d
Chupriarti <wallx13@gmail.com>
2021-12-19 21:47:42 +0300

    add states for modal showing

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -5,29 +5,35 @@ import CreateDevice from '../components/modals/CreateDevice';
 import CreateType from '../components/modals/CreateType';
 
 const Admin = () => {
+    const [isBrandVisible, setBrandVisible] = React.useState(false);
+    const [isTypeVisible, setTypeVisible] = React.useState(false);
+    const [isDeviceVisible, setDeviceVisible] = React.useState(false);
     return (
         <Container className="d-flex flex-column">
             <Button 
                 variant={"outline-dark"} 
                 className="mt-4 p-2"
+                onClick={() => setTypeVisible(true)}
             >
                 Create type
             </Button>
             <Button 
                 variant={"outline-dark"} 
                 className="mt-4 p-2"
+                onClick={() => setBrandVisible(true)}
             >
                 Create brand
             </Button>
             <Button 
                 variant={"outline-dark"} 
                 className="mt-4 p-2"
+                onClick={() => setDeviceVisible(true)}
             >
                 Create device
             </Button>
-            <CreateType show={false} onHide={() => console.log("onHide")} />
-            <CreateBrand show={false} onHide={() => console.log("onHide")} />
-            <CreateDevice show={false} onHide={() => console.log("onHide")} />
+            <CreateType show={isTypeVisible} onHide={() => setTypeVisible(false)} />
+            <CreateBrand show={isBrandVisible} onHide={() => setBrandVisible(false)} />
+            <CreateDevice show={isDeviceVisible} onHide={() => setDeviceVisible(false)} />
         </Container>
     )
 }
