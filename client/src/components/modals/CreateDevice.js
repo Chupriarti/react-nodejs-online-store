5182b7966a16c6c0b6b79bd843d64f00bb385e82
Chupriarti <wallx13@gmail.com>
2021-12-19 21:58:25 +0300

    add selecting device's type

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Button, Form, Modal } from 'react-bootstrap';
+import { Button, Dropdown, Form, Modal } from 'react-bootstrap';
+import { Context } from '../..';
 
 const CreateDevice = ({show, onHide}) => {
+    const {device} = React.useContext(Context);
+
     return (
         <Modal
             show={show}
@@ -16,6 +19,14 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
+                    <Dropdown>
+                        <Dropdown.Toggle>Select type</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.types.map(type => 
+                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>     
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
                     <Form.Control
                         placeholder={"Enter device name"}
                     />
