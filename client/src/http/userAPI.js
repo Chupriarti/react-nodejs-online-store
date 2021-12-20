0da72c814495dca7843cb118b40cc779c9c61cc7
Chupriarti <wallx13@gmail.com>
2021-12-20 21:07:55 +0300

    add login apiUser

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -4,3 +4,8 @@ export const registration = async (email, password) => {
     const response = await $host.post("api/user/registration", {email, password, role: "ADMIN"});
     return response;
 }
+
+export const login = async (email, password) => {
+    const response = await $host.post("api/user/login", {email, password});
+    return response;
+}
