b725f682690081c86c99bc06d60c7a1cfd95148b
Chupriarti <wallx13@gmail.com>
2021-12-20 21:09:59 +0300

    add check apiUser

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -9,3 +9,8 @@ export const login = async (email, password) => {
     const response = await $host.post("api/user/login", {email, password});
     return response;
 }
+
+export const check = async (email, password) => {
+    const response = await $host.post("api/user/registration", {email, password, role: "ADMIN"});
+    return response;
+}
