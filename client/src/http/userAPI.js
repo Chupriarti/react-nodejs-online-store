e09cccc57a0556d09d18dd5a5e7b1ef0c15b29df
Chupriarti <wallx13@gmail.com>
2021-12-20 21:07:17 +0300

    fix

--- client/src/http/userAPI.js
+++ client/src/http/userAPI.js
@@ -2,5 +2,5 @@ import { $authHost, $host } from "./inde";
 
 export const registration = async (email, password) => {
     const response = await $host.post("api/user/registration", {email, password, role: "ADMIN"});
-    return response
+    return response;
 }
