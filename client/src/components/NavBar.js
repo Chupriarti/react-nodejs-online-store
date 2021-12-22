80471f9dc9295ca3a9d42932408d810d49f11542
Chupriarti <wallx13@gmail.com>
2021-12-22 19:58:07 +0300

    add logout logic

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,6 +9,12 @@ import {useNavigate} from 'react-router-dom';
 const NavBar = observer (() => {
     const {user} = React.useContext(Context)
     const navigate = useNavigate();
+
+    const logOut = () => {
+        user.setUser({});
+        user.setIsAuth(false);
+    }
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -24,7 +30,7 @@ const NavBar = observer (() => {
                         <Button 
                             variant={"outline-light"} 
                             className="ms-2"
-                            onClick={() => navigate(LOGIN_ROUTE)}
+                            onClick={() => logOut()}
                         >
                             Logout
                         </Button>
