b7d90fe5a84cd0fd2488d4046407311c63ac1a97
Chupriarti <wallx13@gmail.com>
2021-12-23 21:35:32 +0300

    add fetchBrands api usage

--- client/src/http/deviceAPI.js
+++ client/src/http/deviceAPI.js
@@ -14,3 +14,8 @@ export const createBrand = async (brand) => {
     const {data} = await $authHost.post('api/brand', brand)
     return data
 }
+
+export const fetchBrands = async () => {
+    const {data} = await $host.get('api/brand', )
+    return data
+}
