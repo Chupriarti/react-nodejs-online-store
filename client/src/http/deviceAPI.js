6f7494835f91d4f261eb0489b92a3713354ea62e
Chupriarti <wallx13@gmail.com>
2021-12-23 21:23:25 +0300

    add fetchTypes api usage

--- client/src/http/deviceAPI.js
+++ client/src/http/deviceAPI.js
@@ -4,3 +4,8 @@ export const createType = async (type) => {
     const {data} = await $authHost.post("api/type", type);
     return data;
 }
+
+export const fetchTypes = async () => {
+    const {data} = await $host.post("api/type", type);
+    return data;
+}
