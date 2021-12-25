90a17dc7806fe55fe1ec5abfe860d3da42ec91f7
Chupriarti <wallx13@gmail.com>
2021-12-25 21:50:24 +0300

    add setting name state

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -62,6 +62,8 @@ const CreateDevice = ({show, onHide}) => {
                         </Dropdown.Menu>
                     </Dropdown>
                     <Form.Control
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                         className="mt-3"
                         placeholder={"Enter device name"}
                     />
