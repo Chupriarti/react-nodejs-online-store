f72be65afeaa8da047b9204b4c9b96fd012a3c94
Chupriarti <wallx13@gmail.com>
2021-12-26 20:46:28 +0300

    prepare pagination layout

--- client/src/components/Pages.js
+++ client/src/components/Pages.js
@@ -1,11 +1,17 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
+import { Pagination } from 'react-bootstrap';
+import { Context } from '..';
 
 const Pages = observer(() => {
+    const {device} = React.useContext(Context);
+    const pages = [1, 2, 3, 4, 5];
     return (
-        <div>
-
-        </div>
+        <Pagination className="mt-5">
+            {pages.map(page => 
+                <Pagination.Item>{page}</Pagination.Item>    
+            )}
+        </Pagination>
     )
 });
 
