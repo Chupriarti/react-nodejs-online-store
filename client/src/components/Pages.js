732acbb8159af5ea4d2cadf3bb65cb391b6930c5
Chupriarti <wallx13@gmail.com>
2021-12-26 20:42:32 +0300

    prepare component for pagination

---
+++ client/src/components/Pages.js
@@ -0,0 +1,12 @@
+import { observer } from 'mobx-react-lite';
+import React from 'react';
+
+const Pages = observer(() => {
+    return (
+        <div>
+
+        </div>
+    )
+});
+
+export default Pages;
